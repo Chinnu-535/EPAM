f713eff2015ea4ac2c473eeed17971828303a114
Chinnu-535 <161503055+Chinnu-535@users.noreply.github.com>
2025-03-12 11:19:04 +0530

    react

---
+++ React/app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, React!</h1>
+      <p>React is successfully installed and running.</p>
+    </div>
+  );
+}
+
+export default App;
